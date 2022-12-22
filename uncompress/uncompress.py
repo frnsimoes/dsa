62d7af41e52b50d78f58998e335fda84aef6cfba
frnsimoes <simoes.grcia@gmail.com>
2022-12-21 23:19:28 -0300

    uncompress

---
+++ uncompress/uncompress.py
@@ -0,0 +1,20 @@
+def uncompress(s):
+    result = []
+    i = 0
+    j = 0
+
+    while j < len(s):
+        character = s[j]
+        if character.isnumeric():
+            j += 1  # targets the numeric value index
+        else:
+            number = int(s[i:j])
+            result.append(number * character)
+            j += 1
+            i = j
+
+    return ''.join([x for x in result])
+
+
+if __name__ == '__main__':
+    assert uncompress('2b3c') == 'bbccc'
